053dd5260cf6bb117ad07e4d8e232b190f8b7f01
hossam-zaki <hossam_zaki@brown.edu>
2020-07-06 23:03:14 -0400

    skeletons for voting front-end. Waiting for testing

--- src/main/webapp/voting.js
+++ src/main/webapp/voting.js
@@ -7,6 +7,13 @@ function getInput() {
   fetch("/data?id=" + id)
     .then((response) => response.json())
     .then((stats) => {
-      console.log(stats);
+      document.getElementById("feed").innerHTML = stats.title;
+      var from = document.getElementById("vote-form");
+
+      for (i = 0; i < stats.options.length; i++) {
+        var y = document.createElement("INPUT");
+        y.setAttribute("type", "radio");
+        y.setAttribute("value", stats.option);
+      }
     });
 }
