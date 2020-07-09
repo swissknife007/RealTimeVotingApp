c678da24181609ba37f43def491ffe7e0b9505bf
Hossam A Zaki <hossam_zaki@brown.edu>
2020-07-08 23:14:03 -0400

    multiple options to be displayed and submitted

--- src/main/webapp/voting.js
+++ src/main/webapp/voting.js
@@ -9,18 +9,22 @@ function getInput() {
     .then((stats) => {
       console.log(stats);
       document.getElementById("title").innerHTML = stats.question;
-      var y = document.createElement("INPUT");
-      y.setAttribute("type", "radio");
-      y.setAttribute("value", stats.option);
-      var label = document.createElement("label");
-      label.appendChild(y);
-      label.innerHTML += "<span> " + stats.option + "</span><br>";
-      document.getElementById("vote-form").appendChild(label);
+      // var y = document.createElement("INPUT");
+      // y.setAttribute("type", "radio");
+      // y.setAttribute("value", stats.option);
+      // var label = document.createElement("label");
+      // label.appendChild(y);
+      // label.innerHTML += "<span> " + stats.option + "</span><br>";
+      // document.getElementById("vote-form").appendChild(label);
 
-      // for (i = 0; i < stats.option.length; i++) {
-      //   var y = document.createElement("INPUT");
-      //   y.setAttribute("type", "radio");
-      //   y.setAttribute("value", stats.option);
-      // }
+      for (i = 0; i < stats.option.length; i++) {
+        var y = document.createElement("INPUT");
+        y.setAttribute("type", "radio");
+        y.setAttribute("value", stats.option[i]);
+        var label = document.createElement("label");
+        label.appendChild(y);
+        label.innerHTML += "<span> " + stats.option[i] + "</span><br>";
+        document.getElementById("vote-form").appendChild(label);
+      }
     });
 }
