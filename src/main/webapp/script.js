6c1a88dc0c5910b612fff0e96f24780c711dae82
Tomi Olusina <oolusina@terpmail.umd.edu>
2020-07-17 02:48:49 -0400

    Added functionality for adding a single temporary marker to the map

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -49,13 +49,19 @@ window.onload = function () {
   var map_btn = document.getElementById("map_btn");
   map_btn.onclick = function () {
     if (document.getElementById('map') == null) {
-        var container = document.getElementById("container");
-        var div_map = document.createElement("div");
-        div_map.id = "map";
-        container.appendChild(div_map);
-        map = new google.maps.Map(
-        document.getElementById('map'),
-        {center: {lat: 38.5949, lng: -94.8923}, zoom: 4});
+      var container = document.getElementById("container");
+      var div_map = document.createElement("div");
+      div_map.id = "map";
+      container.appendChild(div_map);
+
+      map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 38.5949, lng: -94.8923}, zoom: 4});
+
+      map.addListener('click', (event) => {
+        createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+      });
+      //fetchMarkers();
   }};
 
   var map_del_btn = document.getElementById("map_del_btn");
@@ -72,16 +78,43 @@ function getInput() {
     });
 }
 
-/** Creates a map that allows users to add markers. */
-function createMap() {
-  map = new google.maps.Map(
-      document.getElementById('map'),
-      {center: {lat: 38.5949, lng: -94.8923}, zoom: 4});
+//Creates a marker that shows a textbox the user can edit
+function createMarkerForEdit(lat, lng) {
+  // If we're already showing an editable marker, then remove it.
+  if (editMarker) {
+    editMarker.setMap(null);
+  }
 
-  // When the user clicks in the map, show a marker with a text box the user can
-  // edit.
-  //map.addListener('click', (event) => {
-    //createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
-  //});
-  //fetchMarkers();
+  editMarker =
+    new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
+}/*
+  const infoWindow =
+    new google.maps.InfoWindow({content: buildInfoWindowInput(lat, lng)});
+
+  // When the user closes the editable info window, remove the marker.
+  google.maps.event.addListener(infoWindow, 'closeclick', () => {
+    editMarker.setMap(null);
+  });
+
+  infoWindow.open(map, editMarker);
 }
+
+//Creates an editable textbox and a submit button for marker
+function buildInfoWindowInput(lat, lng) {
+  const textBox = document.createElement('textarea');
+  const button = document.createElement('button');
+  button.appendChild(document.createTextNode('Submit Marker'));
+
+  button.onclick = () => {
+    postMarker(lat, lng, textBox.value);
+    createMarkerForDisplay(lat, lng, textBox.value);
+    editMarker.setMap(null);
+  };
+
+  const containerDiv = document.createElement('div');
+  containerDiv.appendChild(textBox);
+  containerDiv.appendChild(document.createElement('br'));
+  containerDiv.appendChild(button);
+
+  return containerDiv;
+}*/
